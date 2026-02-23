1e096bac588737c8485beb4abd35f2d60e9b270c
mathankumargsoftsuave <mathankumarg.softsuave@gmail.com>
2026-02-23 18:22:58 +0530

    modify code

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 
-// Master DB (Write operations)
+
+// Master DB → Writes (signup, booking)
 const master = mysql.createConnection({
   host: '172.31.20.141',
   user: 'movieuser',
@@ -8,12 +9,13 @@ const master = mysql.createConnection({
   database: 'movie_app'
 });
 
-// Slave DB (Read operations)
+// Slave DB → Reads (login, get bookings)
 const slave = mysql.createConnection({
   host: '172.31.24.174',
   user: 'root',
   password: 'password',
   database: 'movie_app'
 });
+
 
 module.exports = { master, slave };
